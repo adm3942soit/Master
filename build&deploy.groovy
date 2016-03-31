b6392a0cd6299b95d86d453816191a22932312af
adm3942soit <adm3942soit@gmail.com>
2016-03-31 12:31:06 +0300

    result

--- build&deploy.groovy
+++ build&deploy.groovy
@@ -1,6 +1,10 @@
 node {
     stage 'Checkout'
     git url: 'https://github.com/adm3942soit/Master.git'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    if (matcher) {
+        echo "Building version ${matcher[0][1]}"
+    }
     stage 'Package'
     def mvnHome = tool 'ADOP Maven'
     sh "${mvnHome}/bin/mvn -B verify"
