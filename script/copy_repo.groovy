cd5e7c74c04681143d78300b7568d0e32ba59bd4
adm3942soit <adm3942soit@gmail.com>
2016-04-01 12:48:07 +0300

    script added

--- script/copy_repo.groovy
+++ script/copy_repo.groovy
@@ -2,11 +2,11 @@ node{
     //sh "git clone ssh://jenkins@gerrit:29418/All-Projects"
     //sh "git clone https://github.com/adm3942soit/Master.git"
     //sh "cp -r All-Projects Master"
-    sh "cd /var/jenkins_home/jobs/CopyRepo/workspace/Master/Master"
+    sh "cd var/jenkins_home/jobs/CopyRepo/workspace/Master"
     sh "ls"
     //sh "git add All-Projects"
-    //sh "git commit -a -m"
-    //sh "git push"
+    sh "git commit -a -m"
+    sh "git push"
 
 }
 
