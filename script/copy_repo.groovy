ed26284d0a41d54bacbde141f7e48baceb226577
adm3942soit <adm3942soit@gmail.com>
2016-04-01 12:28:53 +0300

    script added

--- script/copy_repo.groovy
+++ script/copy_repo.groovy
@@ -1,4 +1,4 @@
-node(create_remote_repo){
+node{
     sh "git clone ssh://jenkins@gerrit:29418/All-Projects"
     sh "git clone https://github.com/adm3942soit/Master.git"
     sh "move All-Projects Master"
