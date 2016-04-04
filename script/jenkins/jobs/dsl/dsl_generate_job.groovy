7ac0d7de4f9f21e800bb16e35401839f72d7a212
adm3942soit <adm3942soit@gmail.com>
2016-04-04 16:48:22 +0300

    script added

--- script/jenkins/jobs/dsl/dsl_generate_job.groovy
+++ script/jenkins/jobs/dsl/dsl_generate_job.groovy
@@ -113,7 +113,7 @@ deployJob.with {
     }
     label("docker")
     steps {
-        copyArtifacts("$PARENT_BUILD") {
+        copyArtifacts(${PARENT_BUILD}) {
             buildSelector {
                 buildNumber('${PARENT_BUILD_NUMBER}')
             }
