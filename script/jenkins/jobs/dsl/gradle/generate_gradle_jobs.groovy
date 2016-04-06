5c72ed2b80bccd1511458404d6057ad58a5717a2
adm3942soit <adm3942soit@gmail.com>
2016-04-06 16:38:05 +0300

    script added

--- script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
+++ script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
@@ -1,17 +1,34 @@
+import org.konsuke.github.GitHub
     def theInfoName = "${WORKSPACE}/repositories.txt"
     File theInfoFile = new File(theInfoName)
+    def lines
+    def linesNmbr=0
     if (!theInfoFile.exists()) {
         println "File does not exist!!!!!"
 
     } else {
         println "File exist!!!!!"
+        lines=theInfoFile.
         theInfoFile.eachLine { line ->
             if (line.trim().size() == 0) {
                 println "Null!!!!!"
                 return null
             } else {
                 println "!!!!!!"+"${line}"
+                lines.add("$line")
+                linesNmbr++
             }
 
         }
+    }
+    if(linesNmbr!=0){
+        def i=1
+    lines.each{
+       job{"$i"}{
+           scm{
+              gitHub(lines[$i])
+              i++
+           }
+       }
+    }
     }
