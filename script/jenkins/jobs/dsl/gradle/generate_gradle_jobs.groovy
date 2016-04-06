bcaeb5d7ccacbdcb7ab68a43fd91908c14ae21c7
adm3942soit <adm3942soit@gmail.com>
2016-04-06 14:25:54 +0300

    script added

--- script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
+++ script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
@@ -1,7 +1,7 @@
-def theInfoName = "repositories.txt"
+def theInfoName = "C:/repositories.txt"
 File theInfoFile = new File(theInfoName)
 
-def lines = theInfoFile.readLines()
+//def lines = theInfoFile.readLines()
 
 if (!theInfoFile.exists()) {
     println "File does not exist!!!!!"
