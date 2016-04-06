7adb765ec397df2143e7b8bed22c58fed24a2411
adm3942soit <adm3942soit@gmail.com>
2016-04-06 13:57:06 +0300

    script added

---
+++ script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
@@ -0,0 +1,19 @@
+def theInfoName = "repositories.txt"
+File theInfoFile = new File(theInfoName)
+
+if (!theInfoFile.exists()) {
+    println "File does not exist"
+
+} else {
+
+    theInfoFile.eachLine { line ->
+
+        if (line.trim().size() == 0) {
+            return null
+
+        } else {
+            println "${line}"
+        }
+
+    }
+}
