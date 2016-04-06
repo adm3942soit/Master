2089fb3dc523d368b740971acfb0f395065ff3c5
adm3942soit <adm3942soit@gmail.com>
2016-04-06 16:52:12 +0300

    script added

--- script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
+++ script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
@@ -1,7 +1,7 @@
     def baseName="Gradle-jobs"
     def theInfoName = "${WORKSPACE}/repositories.txt"
     File theInfoFile = new File(theInfoName)
-    def lines
+    def lines = []
     def linesNmbr=0
     if (!theInfoFile.exists()) {
         println "File does not exist!!!!!"
@@ -22,13 +22,55 @@
         }
     }
     if(linesNmbr!=0){
-        def i=1
-    lines.each{
-       job{"$baseName"+"/"+"$i"}{
+        def i=0
+    while(i<=lines.size()){
+       def jobName="$baseName"+"/"+"$i"
+       job{jobName}{
+           wrappers {
+               preBuildCleanup()
+               injectPasswords()
+               maskPasswords()
+               sshAgent("adop-jenkins-master")
+           }
            scm{
-              gitHub(lines[$i])
-              i++
+               git(lines[i])
+               credentials("adop-jenkins-master")
+           }
+           label("java8")
+           triggers{
+               gerrit{
+                   events{
+                       refUpdated()
+                   }
+                   configure { gerritxml ->
+                       gerritxml / 'gerritProjects' {
+                           'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.GerritProject' {
+                               compareType("PLAIN")
+                               //projectFolderName + "/" +
+                               pattern(lines[i])
+                               'branches' {
+                                   'com.sonyericsson.hudson.plugins.gerrit.trigger.hudsontrigger.data.Branch' {
+                                       compareType("PLAIN")
+                                       pattern("master")
+                                   }
+                               }
+                           }
+                       }
+                       gerritxml / serverName("ADOP Gerrit")
+                   }
+               }
+           }
+
+           steps{
+               maven{
+                   mavenInstallation("ADOP Maven")
+                   goals("clean package")
+
+               }
+
            }
        }
+        queue(jobName)
+        i++
     }
     }
