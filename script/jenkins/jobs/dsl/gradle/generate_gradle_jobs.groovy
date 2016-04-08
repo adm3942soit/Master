99b5f8495422797307c6cda89fba2bd8e03700f8
adm3942soit <adm3942soit@gmail.com>
2016-04-08 12:05:40 +0300

    script added

--- script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
+++ script/jenkins/jobs/dsl/gradle/generate_gradle_jobs.groovy
@@ -139,7 +139,7 @@
 
     listView('ListView') {
         jobs {
-           regex(/$baseName(d{0,3})(sonarJob*)/)
+           regex(/$baseName(?d{0,3})(?w{0,8})/)
         }
         columns {
             status()
