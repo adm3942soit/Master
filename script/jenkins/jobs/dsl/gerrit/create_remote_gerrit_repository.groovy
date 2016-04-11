a6911353bb5045d696e733a3e4cab523759ec8da
ad3942soit <ad3942soit@gmail.com>
2016-04-11 12:33:25 +0300

    script added

--- script/jenkins/jobs/dsl/gerrit/create_remote_gerrit_repository.groovy
+++ script/jenkins/jobs/dsl/gerrit/create_remote_gerrit_repository.groovy
@@ -1,6 +1,7 @@
 def nameJob="GerritRepoCopy"
 def PROJECT_NAME="Master"
-def gerritUrl="ssh://jenkins@gerrit:29418/${PROJECT_NAME}"
+def PROJECT_GERRIT_NAME="MasterCopy"
+def gerritUrl="ssh://jenkins@gerrit:29418/${PROJECT_GERRIT_NAME}"
 
 job("$nameJob"){
     scm{
@@ -8,9 +9,12 @@ job("$nameJob"){
     }
     steps{
         shell('''set +x
+        |git clone
         |git remote set-url origin $gerritUrl
         |ssh -p 29418 jenkins@gerrit
         |git clone $gerritUrl
+        |cd $WORKSPACE
+        |ls
        |set -x'''.stripMargin())
     }
     //|git push ssh://jenkins@gerrit:29418/Master HEAD:refs/master
