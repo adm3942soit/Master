7d84daf7931729a2c4b17a05e8c9b5f47688d286
Dudnik, Oksana <oksana.dudnik@accenture.com>
2016-04-28 15:57:12 +0300

    script added

--- script/jenkins/jobs/groovy/jboss_deploy.groovy
+++ script/jenkins/jobs/groovy/jboss_deploy.groovy
@@ -1,3 +1,31 @@
+def jbossHome = "C:\\wildfly-10.0.0.Beta1"
+// Jobs
+//def buildAppJob = freeStyleJob("Master_Build")
+//def deployAppJob = freeStyleJob("Master_Deploy")
+
+node{
+deployApp("target/master.war", jbossHome, 8082)
+}
+
+/*
+buildAppJob.with{
+    scm{
+        git('https://github.com/adm3942soit/Master.git')
+    }
+    steps{
+        maven{
+            mavenInstallation("ADOP Maven")
+            goals("clean package install wildfly:deploy")
+        }
+    }
+}
+
+queue("Master_Build")
+
+deployAppJob.with{
+
+}
+*/
 def deployApp(war, jbossHome, httpPort){
     sh "${jbossHome}/bin/standalone.sh"
     echo "$jbossHome sercer restarted with new webapp $war, see htttp://localhost:$httpPort"
